fc80e1da7a0642ba07675c166d16b9370fd81694
blackhat-coder <abohisrael100@gmail.com>
2020-10-19 19:49:44 +0100

    New

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,11 +3,16 @@ import pandas as pd
 
 
 class LinearRegression:
-
-
     def cost_function(self, theta, target, feautures):
         m = len(target)
 
         cost = np.square(np.dot(feautures, theta) - target).sum()
 
         return cost / (2 * m)
+    
+    def fit(self, X, Y):
+        return np.dot(np.linalg.pinv(np.dot(np.transpose(X) , X)) , np.dot(np.transpose(X) , Y))
+    
+    def predict(self, feautures):
+        return np.dot(feautures, self.fit())
+
